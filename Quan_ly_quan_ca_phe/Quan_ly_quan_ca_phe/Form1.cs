efe2734cc2adbaba94e9313e985e6facbfa38545
nguyen-gia-quang-anh <2251061701@e.tlu.edu.vn>
2024-03-11 21:36:35 +0700

    Tạo project Quản lý quán cà phê

---
+++ Quan_ly_quan_ca_phe/Quan_ly_quan_ca_phe/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Quan_ly_quan_ca_phe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
