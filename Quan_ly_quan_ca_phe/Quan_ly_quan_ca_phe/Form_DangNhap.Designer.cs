bea33e96cc0c299495d86f3d11ab7f880b65d916
nguyen-gia-quang-anh <2251061701@e.tlu.edu.vn>
2024-03-11 23:34:30 +0700

    Form đăng nhập

---
+++ Quan_ly_quan_ca_phe/Quan_ly_quan_ca_phe/Form_DangNhap.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace Quan_ly_quan_ca_phe
+{
+    partial class Form_DangNhap
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tb_username = new System.Windows.Forms.TextBox();
+            this.tb_password = new System.Windows.Forms.TextBox();
+            this.lb_username = new System.Windows.Forms.Label();
+            this.lb_password = new System.Windows.Forms.Label();
+            this.btn_login = new System.Windows.Forms.Button();
+            this.lb_loi = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tb_username
+            // 
+            this.tb_username.Location = new System.Drawing.Point(185, 128);
+            this.tb_username.Name = "tb_username";
+            this.tb_username.Size = new System.Drawing.Size(307, 20);
+            this.tb_username.TabIndex = 0;
+            // 
+            // tb_password
+            // 
+            this.tb_password.Location = new System.Drawing.Point(185, 189);
+            this.tb_password.Name = "tb_password";
+            this.tb_password.Size = new System.Drawing.Size(307, 20);
+            this.tb_password.TabIndex = 1;
+            this.tb_password.UseSystemPasswordChar = true;
+            // 
+            // lb_username
+            // 
+            this.lb_username.AutoSize = true;
+            this.lb_username.Location = new System.Drawing.Point(182, 112);
+            this.lb_username.Name = "lb_username";
+            this.lb_username.Size = new System.Drawing.Size(55, 13);
+            this.lb_username.TabIndex = 2;
+            this.lb_username.Text = "Username";
+            // 
+            // lb_password
+            // 
+            this.lb_password.AutoSize = true;
+            this.lb_password.Location = new System.Drawing.Point(182, 173);
+            this.lb_password.Name = "lb_password";
+            this.lb_password.Size = new System.Drawing.Size(53, 13);
+            this.lb_password.TabIndex = 3;
+            this.lb_password.Text = "Password";
+            // 
+            // btn_login
+            // 
+            this.btn_login.Location = new System.Drawing.Point(272, 249);
+            this.btn_login.Name = "btn_login";
+            this.btn_login.Size = new System.Drawing.Size(130, 35);
+            this.btn_login.TabIndex = 4;
+            this.btn_login.Text = "Đăng nhập";
+            this.btn_login.UseVisualStyleBackColor = true;
+            // 
+            // lb_loi
+            // 
+            this.lb_loi.AutoSize = true;
+            this.lb_loi.Location = new System.Drawing.Point(182, 212);
+            this.lb_loi.Name = "lb_loi";
+            this.lb_loi.Size = new System.Drawing.Size(0, 13);
+            this.lb_loi.TabIndex = 5;
+            // 
+            // Form_DangNhap
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(664, 435);
+            this.Controls.Add(this.lb_loi);
+            this.Controls.Add(this.btn_login);
+            this.Controls.Add(this.lb_password);
+            this.Controls.Add(this.lb_username);
+            this.Controls.Add(this.tb_password);
+            this.Controls.Add(this.tb_username);
+            this.Name = "Form_DangNhap";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Đăng nhập";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tb_username;
+        private System.Windows.Forms.TextBox tb_password;
+        private System.Windows.Forms.Label lb_username;
+        private System.Windows.Forms.Label lb_password;
+        private System.Windows.Forms.Button btn_login;
+        private System.Windows.Forms.Label lb_loi;
+    }
+}
